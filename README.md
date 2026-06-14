f02548c817f8277c55c6a620b355c48de5a789b1
tungtiwi <taquangtung2003@gmail.com>
2026-06-14 14:56:20 +0700

    update fe

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Hệ thống phân tích cảm xúc bình luận
 ## 1. Tạo sinh dữ liệu:
 + Tự tạo mock data gồm 1000 bình luận tiếng việt về sản phẩm xe ô tô bao gồm các nhãn `positive`, `negative`, `neural` đa dạng và thực tế.
 + Bộ dữ liệu datasets được lưu dưới dạng CSV trong `datasets/mock_data_vietnamese_cars.csv`
+
+## 2. Huấn luyện mô hình:
++ Sử dụng kiến trúc TF-IDF kết hợp Logistic Regression.
++ Loại dữ liệu: văn bản ngắn
---
+++ src/frontend/src/App.vue
@@ -0,0 +1,104 @@
+<script setup>
+import { ref } from "vue"
+import axios from "axios"
+
+const text = ref("")
+const result = ref(null)
+const loading = ref(false)
+
+const analyzeSentiment = async () => {
+
+  loading.value = true
+
+  try {
+
+    const response = await axios.post(
+      "http://localhost:5000/predict",
+      {
+        text: text.value
+      }
+    )
+
+    result.value = response.data
+
+  } catch (error) {
+
+    result.value = {
+      status: "error",
+      message:
+        error.response?.data?.message ||
+        "Không kết nối được API"
+    }
+  }
+
+  loading.value = false
+}
+</script>
+
+<template>
+
+  <div class="container">
+
+    <h1>
+      Phân tích cảm xúc bình luận ô tô
+    </h1>
+
+    <textarea
+      v-model="text"
+      placeholder="Nhập bình luận..."
+    />
+
+    <br>
+
+    <button @click="analyzeSentiment">
+
+      Phân tích
+
+    </button>
+
+    <div
+      v-if="loading"
+      class="result"
+    >
+      Đang xử lý...
+    </div>
+
+    <div
+      v-if="result"
+      class="result"
+    >
+
+      <p>
+        <b>Sentiment:</b>
+        {{ result.sentiment }}
+      </p>
+
+      <p>
+        <b>Confidence:</b>
+        {{ result.confidence_score }}
+      </p>
+
+    </div>
+
+  </div>
+
+</template>
+
+<style>
+
+.container{
+  width:700px;
+  margin:auto;
+  margin-top:50px;
+}
+
+textarea{
+  width:100%;
+  height:150px;
+}
+
+.result{
+  margin-top:20px;
+}
+
+</style>
